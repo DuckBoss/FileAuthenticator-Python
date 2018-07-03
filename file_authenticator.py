f77d525fd05d8a4a62c857683add645c0525c33c
Jason (제이슨) <jasonjerome16@gmail.com>
2018-07-03 00:55:29 -0400

    optimized scripts

--- file_authenticator.py
+++ file_authenticator.py
@@ -3,31 +3,27 @@ import sys
 
 if len(sys.argv) != 3:
     print("ERROR: This script requires 3 total system arguments.")
-    print("FORMAT: python file_authenticator.py <src_file> <dst_file>")
+    print("FORMAT: python <this_script.py> <src_file> <dst_file>")
     sys.exit(1)
 
 src_file = sys.argv[1]
 dst_file = sys.argv[2]
 BUFFER_SIZE = 65536  # 64kb chunks
 
-md5_src = hashlib.md5()
-with open(src_file, 'rb') as src:
-    while True:
-        data_src = src.read(BUFFER_SIZE)
-        if not data_src:
-            break
-        md5_src.update(data_src)
 
-md5_dst = hashlib.md5()
-with open(dst_file, 'rb') as dst:
-    while True:
-        data_dst = dst.read(BUFFER_SIZE)
-        if not data_dst:
-            break
-        md5_dst.update(data_dst)
+def file_auth(file_name):
+    md5 = hashlib.md5()
+    with open(file_name, 'rb') as src:
+        while True:
+            data_src = src.read(BUFFER_SIZE)
+            if not data_src:
+                break
+            md5.update(data_src)
+    return md5.hexdigest()
 
-src_digest = md5_src.hexdigest()
-dst_digest = md5_dst.hexdigest()
+
+src_digest = file_auth(src_file)
+dst_digest = file_auth(dst_file)
 
 if src_digest == dst_digest:
     print("The Src file and Dst file is IDENTICAL.")
