fc73b207817f44724ef56d14f034d2c78f91723c
Jason (제이슨) <jasonjerome16@gmail.com>
2018-07-03 01:14:14 -0400

    streamlined scripts

--- file_authenticator.py
+++ file_authenticator.py
@@ -1,5 +1,5 @@
-import hashlib
 import sys
+import auth_api
 
 if len(sys.argv) != 3:
     print("ERROR: This script requires 3 total system arguments.")
@@ -11,19 +11,8 @@ dst_file = sys.argv[2]
 BUFFER_SIZE = 65536  # 64kb chunks
 
 
-def file_auth(file_name):
-    md5 = hashlib.md5()
-    with open(file_name, 'rb') as src:
-        while True:
-            data_src = src.read(BUFFER_SIZE)
-            if not data_src:
-                break
-            md5.update(data_src)
-    return md5.hexdigest()
-
-
-src_digest = file_auth(src_file)
-dst_digest = file_auth(dst_file)
+src_digest = auth_api.file_auth(src_file, BUFFER_SIZE)
+dst_digest = auth_api.file_auth(dst_file, BUFFER_SIZE)
 
 if src_digest == dst_digest:
     print("The Src file and Dst file is IDENTICAL.")
